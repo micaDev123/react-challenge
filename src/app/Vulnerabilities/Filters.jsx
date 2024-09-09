58f1cb5fcbee5c330f423edd4fe868ee446eaadc
Mike <catapangmichaelsuarez@gmail.com>
2024-09-09 23:31:55 +0800

    make some changes and add basic design to be readable on the table

--- src/app/Vulnerabilities/Filters.jsx
+++ src/app/Vulnerabilities/Filters.jsx
@@ -1,97 +1,101 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { Button, CheckLabel, Fieldset, HeadingTwo, Label, Radio } from "ui/library";
 
-export const Filters = ({ searchQuery, setSearchQuery, setSortOrder, setSeverity, setStatus }) => {
-      const [localSearch, setLocalSearch] = useState(searchQuery);
-      const [localSeverity, setLocalSeverity] = useState([]);
-      const [localStatus, setLocalStatus] = useState([]);
+export const Filters = ({ setSearchQuery, setSortOrder, setSeverity, setStatus }) => {
+    const [localSearch, setLocalSearch] = useState("");
+    const [localSeverity, setLocalSeverity] = useState([]);
+    const [localStatus, setLocalStatus] = useState([]);
+    const [sortOrder, setLocalSortOrder] = useState("newest");
 
-  const handleSearchSubmit = (e) => {
+    const handleSearchSubmit = (e) => {
         e.preventDefault();
         setSearchQuery(localSearch);
         setSeverity(localSeverity);
         setStatus(localStatus);
-  };
+        setSortOrder(sortOrder);  // Ensure the parent component updates with the current sort order
+    };
 
-  const handleCheckboxChange = (value, setState, state) => {
-    if (state.includes(value)) {
-    setState(state.filter((item) => item !== value));
-    } else {
-    setState([...state, value]);
-    }
-  };
+    const handleCheckboxChange = (value, setState, state) => {
+        const newState = state.includes(value) ? state.filter(item => item !== value) : [...state, value];
+        setState(newState);
+    };
 
+    const handleSortChange = (event) => {
+        setLocalSortOrder(event.target.value);
+        setSortOrder(event.target.value);  // Ensure immediate update to parent component's state
+    };
 
-return (
-<>
-<HeadingTwo>Filters</HeadingTwo>
-<form onSubmit={handleSearchSubmit}>
-  <Fieldset>
-    <Label htmlFor="search">Search</Label>
-    <div className="flex items-center mb-2">
-      <input
-        className="block border-1 border-gray-300 form-input mr-2 placeholder-gray-400 px-3 py-2 rounded-lg w-full"
-        type="text"
-        id="search"
-        placeholder="e.g., postcss"
-        value={localSearch}
-        onChange={(e) => setLocalSearch(e.target.value)}
-      />
-      <Button type="submit">Search</Button>
-    </div>
-  </Fieldset>
+    return (
+        <>
+            <HeadingTwo>Filters</HeadingTwo>
+            <form onSubmit={handleSearchSubmit}>
+                <Fieldset>
+                    <Label htmlFor="search">Search</Label>
+                    <div className="flex items-center mb-2">
+                        <input
+                            className="block border-1 border-gray-300 form-input mr-2 placeholder-gray-400 px-3 py-2 rounded-lg w-full"
+                            type="text"
+                            id="search"
+                            placeholder="e.g., postcss"
+                            value={localSearch}
+                            onChange={(e) => setLocalSearch(e.target.value)}
+                        />
+                        <Button type="submit">Run</Button>
+                    </div>
+                </Fieldset>
 
-  <Fieldset>
-    <Label>Order by</Label>
-    <CheckLabel>
-      <Radio name="order-by" value="newest" defaultChecked onChange={() => setSortOrder("newest")} />
-      Newest first
-    </CheckLabel>
-    <CheckLabel>
-      <Radio name="order-by" value="oldest" onChange={() => setSortOrder("oldest")} />
-      Oldest first
-    </CheckLabel>
-  </Fieldset>
-  <Fieldset>
-    <Label>Severity</Label>
-    {["High", "Info", "Moderate", "Low", "Critical"].map((level) => (
-      <CheckLabel key={level}>
-        <input
-          type="checkbox"
-          className="form-checkbox mr-2 rounded text-blue-500"
-          onChange={() => handleCheckboxChange(level.toLowerCase(), setLocalSeverity, localSeverity)}
-        />
-        {level}
-      </CheckLabel>
-    ))}
-  </Fieldset>
-  <Fieldset>
-    <Label>Module  Name</Label>
-    {["High", "Info", "Moderate", "Low", "Critical"].map((level) => (
-      <CheckLabel key={level}>
-        <input
-          type="checkbox"
-          className="form-checkbox mr-2 rounded text-blue-500"
-          onChange={() => handleCheckboxChange(level.toLowerCase(), setLocalSeverity, localSeverity)}
-        />
-        {level}
-      </CheckLabel>
-    ))}
-  </Fieldset>
-  <Fieldset>
-    <Label>Status</Label>
-    {["Patched", "Unpatched"].map((statusValue) => (
-      <CheckLabel key={statusValue}>
-        <input
-          type="checkbox"
-          className="form-checkbox mr-2 rounded text-blue-500"
-          onChange={() => handleCheckboxChange(statusValue.toLowerCase(), setLocalStatus, localStatus)}
-        />
-        {statusValue}
-      </CheckLabel>
-    ))}
-  </Fieldset>
-</form>
-</>
-);
+                <Fieldset>
+                    <Label>Order by</Label>
+                    <CheckLabel>
+                        <Radio 
+                            name="order-by" 
+                            value="newest" 
+                            onChange={handleSortChange}
+                            checked={sortOrder === "newest"}
+                        />
+                        Newest first
+                    </CheckLabel>
+                    <CheckLabel>
+                        <Radio 
+                            name="order-by" 
+                            value="oldest"
+                            onChange={handleSortChange}
+                            checked={sortOrder === "oldest"}
+                        />
+                        Oldest first
+                    </CheckLabel>
+                </Fieldset>
+
+                <Fieldset>
+                    <Label>Severity</Label>
+                    {["High", "Info", "Moderate", "Low", "Critical"].map((level) => (
+                        <CheckLabel key={level}>
+                            <input
+                                type="checkbox"
+                                className="form-checkbox mr-2 rounded text-blue-500"
+                                onChange={() => handleCheckboxChange(level.toLowerCase(), setLocalSeverity, localSeverity)}
+                                checked={localSeverity.includes(level.toLowerCase())}
+                            />
+                            {level}
+                        </CheckLabel>
+                    ))}
+                </Fieldset>
+
+                <Fieldset>
+                    <Label>Status</Label>
+                    {["Patched", "Unpatched"].map((statusValue) => (
+                        <CheckLabel key={statusValue}>
+                            <input
+                                type="checkbox"
+                                className="form-checkbox mr-2 rounded text-blue-500"
+                                onChange={() => handleCheckboxChange(statusValue.toLowerCase(), setLocalStatus, localStatus)}
+                                checked={localStatus.includes(statusValue.toLowerCase())}
+                            />
+                            {statusValue}
+                        </CheckLabel>
+                    ))}
+                </Fieldset>
+            </form>
+        </>
+    );
 };
--- src/app/Vulnerabilities/Vulnerabilities.jsx
+++ src/app/Vulnerabilities/Vulnerabilities.jsx
@@ -1,36 +1,25 @@
-import React,{useState,useEffect} from "react";
-import {Filters} from "./Filters";
+import React, { useState, useEffect } from "react";
+import { Filters } from "./Filters";
 import advisoriesData from "../../data/npm-advisories.json";
-import {HeadingOne, HeadingThree, Small, Tag, Table, Thead, Tbody, Tr, Th, Td} from "ui/library";
+import { HeadingOne, Small, Tag, Table, Thead, Tbody, Tr, Th, Td } from "ui/library";
 
 export const Vulnerabilities = () => {
-      const [advisories, setAdvisories] = useState(advisoriesData);
-      const [searchTerm, setSearchTerm] = useState(localStorage.getItem("searchQuery") || "");
-      const [sortOrder, setSortOrder] = useState(localStorage.getItem("sortOrder") || "newest");
-      const [severityFilter, setSeverityFilter] = useState(JSON.parse(localStorage.getItem("severityFilter")) || []);
-      const [statusFilter, setStatusFilter] = useState(JSON.parse(localStorage.getItem("statusFilter")) || []);
+  const [advisories, setAdvisories] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortOrder, setSortOrder] = useState("newest");
+  const [severityFilter, setSeverityFilter] = useState([]);
+  const [statusFilter, setStatusFilter] = useState([]);
 
   useEffect(() => {
-    
-    let filteredAdvisories = [...advisoriesData];
+    let filteredAdvisories = advisoriesData.filter(advisory => {
+      const moduleMatch = advisory.module_name.toLowerCase().includes(searchTerm.toLowerCase());
+      const titleMatch = advisory.title.toLowerCase().includes(searchTerm.toLowerCase());
+      const severityMatch = severityFilter.length === 0 || severityFilter.includes(advisory.severity.toLowerCase());
+      const statusMatch = statusFilter.length === 0 || statusFilter.includes(advisory.patched_versions ? "patched" : "unpatched");
+      return (moduleMatch || titleMatch) && severityMatch && statusMatch;
+    });
 
-    if(searchTerm){
-      filteredAdvisories = filteredAdvisories.filter((advisory) =>
-        advisory.module_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        advisory.title.toLowerCase().includes(searchTerm.toLowerCase())
-      );
-    }
-    if(severityFilter.length > 0){
-      filteredAdvisories = filteredAdvisories.filter((advisory) =>
-        severityFilter.includes(advisory.severity.toLowerCase())
-      );
-    }
-    if(statusFilter.length > 0){
-      filteredAdvisories = filteredAdvisories.filter((advisory) =>
-        statusFilter.includes(advisory.patched_versions ? "patched" : "unpatched")
-      );
-    }
-    if(sortOrder === "newest"){
+    if (sortOrder === "newest") {
       filteredAdvisories.sort((a, b) => new Date(b.created) - new Date(a.created));
     } else {
       filteredAdvisories.sort((a, b) => new Date(a.created) - new Date(b.created));
@@ -39,59 +28,51 @@ export const Vulnerabilities = () => {
     setAdvisories(filteredAdvisories);
   }, [searchTerm, severityFilter, statusFilter, sortOrder]);
 
-return (
-<div className="md:flex md:flex-row-reverse">
-  <div className="flex-shrink-0 md:flex-auto md:max-w-xs mb-4 md:mb-0 md:ml-3 p-4 md:p-6">
-    <Filters
-      searchQuery={searchTerm}
-      setSearchQuery={setSearchTerm}
-      setSortOrder={setSortOrder}
-      setSeverity={setSeverityFilter}
-      setStatus={setStatusFilter}
-    />
-  </div>
-
-  <div className="flex-1 md:flex-auto">
-    <div className="bg-white border border-gray-300 mx-auto p-4 md:p-6 rounded-lg lg:rounded-xl shadow-sm lg:shadow-md">
-      <div className="mb-4">
-        <HeadingOne>Security advisories</HeadingOne>
-        <Small>Showing {advisories.length} items</Small>
+  return (
+    <div className="md:flex md:flex-row-reverse">
+      <div className="flex-shrink-0 md:flex-auto md:max-w-xs mb-4 md:mb-0 md:ml-3 p-4 md:p-6">
+        <Filters
+          setSearchQuery={setSearchTerm}
+          setSortOrder={setSortOrder}
+          setSeverity={setSeverityFilter}
+          setStatus={setStatusFilter}
+        />
       </div>
 
-      {advisories.length === 0 ? (
-        <p>No advisories found matching your search criteria.</p>
-      ) : (
-        <Table>
-          <Thead>
-            <Tr>
-              <Th textAlign="left">Advisory</Th>
-              <Th>Date of advisory</Th>
-              <Th>Status</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {advisories.map((advisory) => (
-              <Tr key={advisory.id}>
-                <Td>
-                  <Small className="block mb-0.5 font-bold">{advisory.module_name}</Small>
-                  <HeadingThree className="mb-1">{advisory.title}</HeadingThree>
-                  <Tag theme="success" title={`Severity: ${advisory.severity}`}>{advisory.severity}</Tag>
-                </Td>
-                <Td textAlign="center">
-                  <p className="text-gray-600">{new Date(advisory.created).toLocaleDateString()}</p>
-                </Td>
-                <Td textAlign="center">
-                  <Tag theme={advisory.patched_versions ? "info" : "danger"} title={`Status: ${advisory.patched_versions ? "Patched" : "Unpatched"}`}>
-                    {advisory.patched_versions ? "Patched" : "Unpatched"}
-                  </Tag>
-                </Td>
+      <div className="flex-1 md:flex-auto">
+        <div className="bg-white border border-gray-300 mx-auto p-4 md:p-6 rounded-lg lg:rounded-xl shadow-sm lg:shadow-md">
+          <div className="mb-4">
+            <HeadingOne>Security Advisories</HeadingOne>
+            <Small>Showing {advisories.length} items</Small>
+          </div>
+          <Table>
+            <Thead>
+              <Tr>
+                <Th>Title</Th>  {/* Updated Heading */}
+                <Th>Module Name</Th>
+                <Th>Serverity</Th>
+                <Th>Advisory Date</Th>       {/* Updated Heading */}
+                <Th>Status</Th>              {/* Existing Heading */}
               </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      )}
+            </Thead>
+            <Tbody>
+              {advisories.map((advisory) => (
+                <Tr key={advisory.id}>
+                  <Td>{advisory.title}</Td> {/* Combined title and module name */}
+                  <Td>{advisory.module_name}</Td>
+                  <Td>{advisory.severity}</Td>
+                  <Td>{new Date(advisory.created).toLocaleDateString()}</Td>
+                  <Td>
+                    <Tag theme={advisory.patched_versions ? "info" : "danger"} title={`Status: ${advisory.patched_versions ? "Patched" : "Unpatched"}`}>
+                      {advisory.patched_versions ? "Patched" : "Unpatched"}
+                    </Tag>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-);
+  );
 };
